6c6195f717c5b7342ca7032c8072c6d3868f0c42
Hadžo Otuzbir <hadzootuzbir@Hadzos-MacBook-Air-2.local>
2023-05-23 14:27:54 +0200

    user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,95 +1,3 @@
-// const User = require("../models/User");
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require('bcrypt');
-// const { BadRequestError } = require('../errors')
-// require('dotenv').config();
-
-
-// const accessTokenMaxAge = 15 * 60;
-// const refreshTokenMaxAge = 7 * 24 * 60 * 60;
-// const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
-// const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
-// console.log(process.env.ACCESS_TOKEN_SECRET);
-
-
-// const generateAccessToken = (user) => {
-//   return jwt.sign({ _id: user._id, email: user.email }, accessTokenSecret, {
-//     expiresIn: accessTokenMaxAge,
-//   });
-// };
-
-
-// const generateRefreshToken = (id) => {
-//   return jwt.sign({ id }, refreshTokenSecret, {
-//     expiresIn: refreshTokenMaxAge,
-//   });
-// };
-
-// const handleErrors = (err) => {
-//   let errors = { email: "", password: "" };
-
-//   console.log(err);
-//   if (err.message === "incorrect email") {
-//     errors.email = "That email is not registered";
-//   }
-
-//   if (err.message === "incorrect password") {
-//     errors.password = "That password is incorrect";
-//   }
-
-//   if (err.code === 11000) {
-//     errors.email = "Email is already registered";
-//     return errors;
-//   }
-
-//   if (err.message.includes("Users validation failed")) {
-//     Object.values(err.errors).forEach(({ properties }) => {
-//       errors[properties.path] = properties.message;
-//     });
-//   }
-
-//   return errors;
-// };
-
-// User.login = async function(email, password) {
-//   const user = await this.findOne({ email });
-
-//   if (user) {
-//     const auth = await bcrypt.compare(password, user.password);
-//     if (auth) {
-//       return user;
-//     }
-//     throw new Error("incorrect password");
-//   }
-//   throw new Error("incorrect email");
-// };
-
-
-// module.exports.login = async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     const user = await User.login(email, password);
-//     const accessToken = generateAccessToken(user._id);
-//     const refreshToken = generateRefreshToken(user._id);
-//     const token = generateAccessToken(user);
-
-//     res.cookie("refreshToken", refreshToken, { httpOnly: true, maxAge: refreshTokenMaxAge * 1000 });
-//     res.status(201).json({ 
-//       status: true, 
-      
-//         id: user._id,
-//         firstName: user.firstName,
-//         lastName: user.lastName,
-//         email: user.email,
-//         role: user.role,
-    
-//       token 
-//     });
-//   } catch (err) {
-//     const errors = handleErrors(err);
-//     res.status(401).json({ errors, status: false, msg: 'Imas neku gresku' });
-//   }
-// };
 
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
@@ -97,7 +5,7 @@ const bcrypt = require('bcrypt');
 const { BadRequestError } = require('../errors')
 require('dotenv').config();
 
-const accessTokenMaxAge = 15 * 60;
+const accessTokenMaxAge = 60 * 60;
 const refreshTokenMaxAge = 7 * 24 * 60 * 60;
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
