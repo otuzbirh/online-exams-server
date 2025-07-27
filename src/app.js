53c5dc4c6f92fbf793c5dd14a4e9d0d540e012ee
Hadžo Otuzbir <hadzo.1999@gmail.com>
2025-07-27 13:04:38 +0200

    change node version

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
     app.listen(port, () =>
-      console.log(`Server is listening testic on port ${port}...`)
+      console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
     console.log(error);
